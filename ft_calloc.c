261b183f226455efa7f176abeb9c291c78dd30bd
markjso <97271827+markjso@users.noreply.github.com>
2022-03-23 12:09:25 +1030

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,15 +11,21 @@
 /* ************************************************************************** */
 /*
 ** Allocates the requested memory and returns a pointer to it.
-** Sets the allocated memory to zero. Returns null if the request fails.
+** Sets the allocated memory to zero using the ft_bzero function. 
+** Returns NULL if the request fails.
 */
 
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	//use a void pointer here of ptr so that any type of data can be applied
 	void	*ptr;
 
+	// allocte memory based on the count multipled by size variables in our parameter
+	// if the memory allocation fails return NULL.
+	// otherwise run bzero function on the allocated zone of memory ptr and give it our size times count parameter
+	// return the void variable ptr
 	ptr = malloc(size * count);
 	if (ptr == NULL)
 		return (0);
