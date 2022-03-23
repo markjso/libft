62bf8095c4603dfd0889ed4055d97e76dbbbd8f0
markjso <97271827+markjso@users.noreply.github.com>
2022-03-23 14:19:02 +1030

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,21 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 /*
-** Copies n characters from memory area src to memoray area dst.
+** Copies n bytes from memory area src to memoray area dst. Returns the value of dst.
 */
 #include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *restrict src, size_t n)
 {
+	//make three variables, i is the counter to perform the loop and move through the strings
+	//and two char pointer variables to cast our src and dst to
+	// set the counter to 0 and cast the paratmer strings to the char pointers csrc and cdst
 	size_t	i;
 	char	*csrc;
 	char	*cdst;
 
-	if (!dst && !src)
-		return (0);
 	i = 0;
 	csrc = (char *)src;
 	cdst = (char *)dst;
+	// as long as the counter i is less than n the loop continues
+	// i acts as the index postion and places the index postion of csrc into cdst
+	// increment and continue until i is no longer less than n and then return dst
 	while (i < n)
 	{
 		cdst[i] = csrc[i];
