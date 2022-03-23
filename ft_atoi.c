0a8fd9cbf076e981a526813deae9a854d5ca66c6
markjso <97271827+markjso@users.noreply.github.com>
2022-03-23 11:42:52 +1030

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -24,21 +24,30 @@ int	ft_atoi(const char *str)
 	res = 0;
 	sign = 1;
 	i = 0;
+	// skip over any kind of spacing that might be at the start of the string
 	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
 		|| str[i] == '\t' || str[i] == '\v' || str[i] == '\r')
 	{
 		i++;
 	}
+	//check to see if there is a negative symbol at the beginning
+	//if negative we set sign to equal -1 to multiply our result when we return it
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign = -1;
 			i++;
 	}
+	// convert the string from char to int if they are numbers
+	// first multiply res by 10 to set up the digit
+	// then subtract the numerical value of the character '0' on the ascic table (48)	
+	// this sets it to ascii numerical value 
+	// Loop until we hit a character that is not a number
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		res = (str[i] - '0') + (res * 10);
+		res = res * 10 + str[i] - '0';
 			i++;
 	}
+	// return res multiplied by sign to return a positive or negative number (int)
 	return (res * sign);
 }
