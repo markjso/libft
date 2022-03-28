2ff8e10579f0b0acfd38e485f7c51ae0efd6652d
markjso <97271827+markjso@users.noreply.github.com>
2022-03-28 14:56:52 +1030

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,13 +18,22 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	// declare a counter variable of i
 	size_t			i;
 	unsigned char	*src;
 	unsigned char	*dst;
 
 	i = 0;
+	// cast the constant char s1 and s2 to unsigned chars
 	src = (unsigned char *)s1;
 	dst = (unsigned char *)s2;
+	// while i is less than n or we are not at the end of src or dst
+	// check to see if the character at position i in src is the same
+	// as the character at position i in dst and if so continue the loop
+	// and return 0 if you get to the end
+	// else if it is not then return the difference between the current
+	// character in src at position i from the current character in dest at
+	// position i
 	while ((i < n) && (src[i] != '\0' || dst[i] != '\0'))
 	{
 		if (dst[i] == src[i])
