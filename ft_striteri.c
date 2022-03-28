2df451f3ca7e0b70a06fa70c0ad78672762a5f94
markjso <97271827+markjso@users.noreply.github.com>
2022-03-28 12:21:31 +1030

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -11,7 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+// this function applies the function parameter f to each character of a string 
+// passesed in at a specific index postion from the parameter s
 void	ft_striteri(char *s, void (*f) (unsigned int, char *))
 {
 	unsigned int	i;
@@ -19,6 +20,9 @@ void	ft_striteri(char *s, void (*f) (unsigned int, char *))
 	if (s != 0 && f != 0)
 	{
 		i = 0;
+		// as long as we are not at the end of the string and our function is not null
+		// we start at the beginningof the string and work our way through, applying
+		// the function f to each character. 
 		while (s[i])
 		{
 			f(i, s + i);
