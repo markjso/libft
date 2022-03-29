345948d8ee52e695728d059c40772f47e74eff56
markjso <97271827+markjso@users.noreply.github.com>
2022-03-29 14:41:14 +1030

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -27,11 +27,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	size_t	i;
 	size_t	j;
 
+	i = 0;
 	if (!needle[0])
 		return ((char *)haystack);
 	if (needle == haystack)
-		return (0);
-	i = 0;
+		return (NULL);
+	if (n == 0)
+		return (NULL);
 	while (haystack[i] && i < n)
 	{
 		j = 0;
@@ -42,7 +44,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 			return ((char *)haystack + i);
 	i++;
 	}
-	return (0);
+	return (NULL);
 }
 /* 
 #include <stdio.h>
