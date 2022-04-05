7f5355b52827bac8ca0362de1a066a9a0df31588
markjso <97271827+markjso@users.noreply.github.com>
2022-04-05 16:07:14 +0930

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -22,30 +22,29 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
+     size_t  h;
+     size_t  n;
 
-	i = 0;
-	if (!needle[0])
-		return ((char *)haystack);
-	if (needle == haystack)
-		return (NULL);
-	if (n == 0)
-		return (NULL);
-	while (haystack[i] && i < n)
-	{
-		j = 0;
-		while (haystack [i + j] && needle[j] && i + j < n
-			&& haystack[i + j] == needle[j])
-		j++;
-		if (!needle[j])
-			return ((char *)haystack + i);
-	i++;
-	}
-	return (NULL);
-}
+     h = 0;
+     if (ft_strlen(needle) == '\0')
+         return ((char *)haystack);
+     while (haystack[h] != '\0')
+     {
+         n = 0;
+         while (haystack[h + n] == needle[n] && (h + n) < len)
+         {
+             if (haystack[h + n] == '\0' && needle[n] == '\0')
+             return ((char*)&haystack[h]);
+         n++;
+         }
+     if (needle[n] == '\0')
+         return ((char*)haystack + h);
+     h++;
+     }
+     return (NULL);
+ }
 /* 
 #include <stdio.h>
 int main()
