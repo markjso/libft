11b395ad9948baf0df7601c4084f1ccb9113fcd2
markjso <97271827+markjso@users.noreply.github.com>
2022-04-11 10:14:02 +0930

    Update libft.h

--- libft.h
+++ libft.h
@@ -33,14 +33,11 @@ char			*ft_strchr(const char *str, int c);
 char			*ft_strstr(const char *haystack, const char *needle, size_t n);
 int				ft_isalpha(int c);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t			ft_strlcat(char *restrict dst,
-					const char *restrict src, size_t n);
+size_t			ft_strlcat(char *dst, const char *src, size_t n);
 char			*ft_strrchr(const char *str, int c);
 int				ft_isascii(int c);
-void			*ft_memcpy(void *dst, const void *restrict src,
-					size_t n);
-size_t			ft_strlcpy(char *restrict dst, const char *restrict src,
-					size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+size_t			ft_strlcpy(char *dst, const char *src, size_t n);
 int				ft_tolower(int c);
 void			*ft_calloc(size_t count, size_t size);
 char			*ft_strdup(const char *s1);
