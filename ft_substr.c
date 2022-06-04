172b5f2b29e6a8a93009825adbd3852163f3fb33
markjso <97271827+markjso@users.noreply.github.com>
2022-06-04 18:27:35 +0930

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -29,7 +29,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	j = 0;
 	str = (char *)malloc(sizeof(*s) * (len + 1));
 	// if the allocation fails return NULL
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 	
 	// until we reach the end of string s, if i is greater than start and j is less 
