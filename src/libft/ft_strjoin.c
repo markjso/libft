5d4eb30b8221571d9cc901b9bd043b8f189d7537
markjso <97271827+markjso@users.noreply.github.com>
2022-06-08 16:04:50 +0930

    Update ft_strjoin.c

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -25,27 +25,28 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
-	i = 0;
-	j = 0;
 	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	// if the allocation fails return NULL
 	// if not using i as the counter as long as we have not reached end of string s1 
 	// place each character of s1 into dest until it is has reached the end then go to the next loop
-	if (!dest)
+	if (!s1 || !s2 ||!dest)
 		return (NULL);
+	i = 0;
+	j = 0;
 	while (s1[i])
 	{
 		dest[i] = s1[i];
 		i++;
 	}
-	// using counter j to place each character of s2 into dest at position i + j. This means it will start at
+	// using counter j to place each character of s2 into dest at position i. This means it will start at
 	// the end of the previous string and join this one to it. Once it reaches the end add a terminating '\0'
-	// and return the joined string dest
+	// and return the joined string dest.
 	while (s2[j])
 	{
-		dest[i + j] = s2[j];
+		dest[i] = s2[j];
+		i++;
 		j++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
