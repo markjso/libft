f5c590ddf957d7fce58b1a9db8826003e228c0eb
markjso <97271827+markjso@users.noreply.github.com>
2022-06-28 17:16:40 +0930

    Update ft_memchr.c

--- src/libft/ft_memchr.c
+++ src/libft/ft_memchr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 /*
-** Locates the first occurrence of c (converted to unsigned char) in string s.
+** Locates the first occurrence of c (cast to unsigned char) in string s.
 ** Returns a pointer to the byte located, or NULL if no such byte exists
 ** within n bytes.
 */
@@ -22,10 +22,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	i = 0;
 	// i = 0 so that we start at the beginning or our string s
+	// if i is not less than n return NULL
 	// while our i variable is less than the parameter n our loop will continue
-	// cast our void s to a char s and check if the current index postion is equal to c
-	// if true will return a pointer to the indes position that contains c
-	// if is no longer less than n return NULL
+	// cast our void s to a char and check if the current index postion is equal to c
+	// if true will return a pointer to the index position that contains c and the rest of the string
+	// else loop until i is no longer less than n and return NULL
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
@@ -34,3 +35,16 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+/*
+** int main () {
+**    const char s[] = "https://www.awebsite.com.au";
+**    const char ch = '.';
+**    char *result;
+**
+**    result = memchr(s, ch, 11);
+**
+**    printf("String after character is %s\n", result);
+**
+**    return(0);
+** }
+*/
