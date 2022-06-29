a7d1bf152b4cb81e4094d87349c5d014508e27b1
markjso <97271827+markjso@users.noreply.github.com>
2022-06-29 12:22:11 +0930

    Update ft_strchr.c

--- src/libft/ft_strchr.c
+++ src/libft/ft_strchr.c
@@ -19,13 +19,32 @@
 
 char	*ft_strchr(const char *str, int c)
 {
+	// while *str is not null the loop continues
+	// if the variable in int c is found in *str
+	// if int c is not found and it is not the end of str
+	// increment str. when int c is found in str return str from this point on. 
 	while(*str)
 	{
 		if (*str == (char) c)
 		    return ((char *)str);
 		str++;
 	}
+	// if you reach the end of the string and the variable in int c
+	// is found then return str from this point on
+	// otherwise if int c is not found return null
 	if (*str == (char) c)
 		return ((char *)str);
 	return (NULL);
 }
+/*
+int main () {
+   const char str[] = "Today. Tonight. Always";
+   const char ch = '.';
+   char *ret;
+
+   ret = ft_strchr(str, ch);
+
+   printf("String after |%c| is = %s\n", ch, ret);
+   
+   return(0);
+}*/
