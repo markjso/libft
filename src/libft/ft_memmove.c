08b814b7dd5901a98e928e37a149f327044a671b
markjso <97271827+markjso@users.noreply.github.com>
2022-06-29 11:23:42 +0930

    Update ft_memmove.c

--- src/libft/ft_memmove.c
+++ src/libft/ft_memmove.c
@@ -21,9 +21,9 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	// we call a variable of i to compare to n
-	// if the length of dst string is greater than src string then while n is greater than zero
-	// the char cast of dst = the char cast of src starting from the end of the string
-	// decrement n until it is less than zero
+	// if the length of dst string is greater than src string and while n is greater than zero
+	// go to the index position of the end of the src string and copy this into the same position
+	// of the dst string. decrement until n is less than zero (or the start of the src string)
 	// return dst
 	
 	if (dst > src)
@@ -35,9 +35,11 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		}
 	}
 	// else i is set to 0 or the start of the string
-	// as long as is is less than n the loop continues
-	// if i is less than n than the char cast of dst = the char cast of src starting from the first character
-	// return dst
+	// while i is less than n the loop continues
+	// the char cast of src is copied into the char cast of dst starting
+	// from the first character up to the lenght of n. Return dst.
+	// if the value of n is less than the length of the string dst
+	// then the remaining characters in the string will also be returned.
 	else
 	{
 		i = 0;
@@ -48,4 +50,15 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		}
 	}
 	return (dst);
+}
+
+int main () {
+   char dst[] = "the dest";
+   const char src[]  = "our src string";
+
+   printf("Before memmove dest = %s, src = %s\n", dst, src);
+   ft_memmove(dst, src, 9);
+   printf("After memmove dest = %s, src = %s\n", dst, src);
+
+   return(0);
 }
