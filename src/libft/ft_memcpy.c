a2782ae9001d90b5f548ac9c855687d1c3c2de87
markjso <97271827+markjso@users.noreply.github.com>
2022-06-29 11:01:25 +0930

    Update ft_memcpy.c

--- src/libft/ft_memcpy.c
+++ src/libft/ft_memcpy.c
@@ -16,7 +16,7 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	//make three variables, i is the counter to perform the loop and move through the strings
+	//initialise three variables, i is the counter to perform the loop and move through the strings
 	//and two char pointer variables to cast our src and dst to
 	// set the counter to 0 and cast the paratmer strings to the char pointers csrc and cdst
 	size_t	i;
@@ -26,9 +26,11 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	i = 0;
 	csrc = (char *)src;
 	cdst = (char *)dst;
-	// as long as the counter i is less than n the loop continues
-	// i acts as the index postion and places the index postion of csrc into cdst
-	// increment and continue until i is no longer less than n and then return dst
+	// while is less than n enter the loop
+	// i acts as the index postion and places the character at index postion of csrc into cdst
+	// increment and continue until i is no longer less than n and then return cdst
+	// if size of bytes copied from the src string is more than the dst string then it will only return these
+	// else n bytes of the src string will be copied and the remaining characters in dst after n bytes will also be returned
 	while (i < n)
 	{
 		cdst[i] = csrc[i];
@@ -36,3 +38,13 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (cdst);
 }
+/*
+int main () {
+   const char src[15] = "this is a string;
+   char dest[15] = "Heloooo!!";
+   printf("Before memcpy dest = %s\n", dest);
+   ft_memcpy(dest, src, 5);
+   printf("After memcpy dest = %s\n", dest);
+   
+   return(0);
+}*/
