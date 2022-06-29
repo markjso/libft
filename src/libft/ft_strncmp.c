090a19bfb9c483330e5d1c09b0b4cad52d5d05e5
markjso <97271827+markjso@users.noreply.github.com>
2022-06-29 17:07:07 +0930

    Update ft_strncmp.c

--- src/libft/ft_strncmp.c
+++ src/libft/ft_strncmp.c
@@ -12,7 +12,7 @@
 /*
 ** Compares at most the first n bytes of s1 and s2.
 ** Returns value 0 if bytes are equal.
-** Else returns < or > 0 difference.
+** Else returns < or > 0 the ascii difference.
 */
 #include "libft.h"
 
@@ -29,11 +29,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	dst = (unsigned char *)s2;
 	// while i is less than n or we are not at the end of src or dst
 	// check to see if the character at position i in src is the same
-	// as the character at position i in dst and if so continue the loop
-	// and return 0 if you get to the end
-	// else if it is not then return the difference between the current
-	// character in src at position i from the current character in dest at
-	// position i
+	// as the character at position i in dst and if so continue the loop 
+	// until it breaks and return 0 if you get to the end of the string
+	// else if the character is not the same then return the difference (in ascii) 
+	// between the current character in src at position i from the current 
+	// character in dest at position i
 	while ((i < n) && (src[i] != '\0' || dst[i] != '\0'))
 	{
 		if (dst[i] == src[i])
@@ -45,3 +45,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
+/*
+int main() {
+  char s1[] = "abcd", s2[] = "abCd", s3[] = "abcd";
+  int result;
+
+  result = ft_strncmp(s1, s2, 4);
+  printf("strcmp(s1, s2) = %d\n", result);
+
+  result = ft_strncmp(s1, s3, 4);
+  printf("strcmp(s1, s3) = %d\n", result);
+
+  return 0;
+}*/
